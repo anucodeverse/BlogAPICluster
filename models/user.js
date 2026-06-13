2d15bb10b1792972d92c87a9bbfe9cbe5952ea83
Ananthalakshmi <surlaanthalakshmi@gmail.com>
2026-06-13 19:54:27 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,15 +1,12 @@
 const mongoose = require("mongoose");
-
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
   },
-
-  email: {
+ email: {
     type: String,
     required: true,
   },
 });
-
 module.exports = mongoose.model("User", userSchema);
