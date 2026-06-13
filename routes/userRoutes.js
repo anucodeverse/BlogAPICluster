36e4bf9496ebc64ec199ee6b881d8458618720ae
Ananthalakshmi <surlaanthalakshmi@gmail.com>
2026-06-13 20:03:08 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,11 @@
 const express = require("express");
-
 const router = express.Router();
-
 const {
   createUser,
   getUsers,
   updateUser,
 } = require("../controllers/userController");
-
 router.post("/", createUser);
-
 router.get("/", getUsers);
-
 router.put("/:id", updateUser);
-
 module.exports = router;
