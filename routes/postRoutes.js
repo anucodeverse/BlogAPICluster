7c6f9db5e765275a62c631fe4a2fe837f28a5b2e
Ananthalakshmi <surlaanthalakshmi@gmail.com>
2026-06-13 20:02:04 +0530

    Update postRoutes.js

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,20 +1,13 @@
 const express = require("express");
-
 const router = express.Router();
-
 const {
   createPost,
   getPosts,
   deletePost,
   getRecentPosts,
 } = require("../controllers/postController");
-
 router.post("/", createPost);
-
 router.get("/", getPosts);
-
 router.delete("/:id", deletePost);
-
 router.get("/recent", getRecentPosts);
-
 module.exports = router;
