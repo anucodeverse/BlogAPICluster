76c17aef3c937c97df41d3b3d671f9c6bd543054
anucodeverse <surlaanthalakshmi.2004@gmail.com>
2026-06-20 22:17:15 +0530

    fix multer export bug

--- middleware/upload.js
+++ middleware/upload.js
@@ -3,8 +3,6 @@ const multer = require("multer");
 // store file in memory
 const storage = multer.memoryStorage();
 
-const upload = multer({
-  storage,
-});
+const upload = multer({ storage });
 
 module.exports = upload;
