1786600c86bef5a4b9a1ceef2f6b25cf5d706ab1
anucodeverse <surlaanthalakshmi.2004@gmail.com>
2026-06-20 20:18:21 +0530

    fix image upload issue

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,36 +1,10 @@
 const multer = require("multer");
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("../config/cloudinary");
 
-const storage = new CloudinaryStorage({
-  cloudinary,
-  params: {
-    folder: "sprint11-frontend",
-    allowed_formats: ["jpg", "jpeg", "png"],
-    resource_type: "image",
-  },
-});
-const cloudinary = require("../config/cloudinary");
-
-const uploadToCloudinary = (buffer) => {
-  return new Promise((resolve, reject) => {
-    cloudinary.uploader
-      .upload_stream(
-        { folder: "sprint11-frontend" },
-        (error, result) => {
-          if (error) return reject(error);
-          resolve(result);
-        }
-      )
-      .end(buffer);
-  });
-};
-const multer = require("multer");
-
+// store file in memory
 const storage = multer.memoryStorage();
 
-const upload = multer({ storage });
-
-module.exports = upload;
+const upload = multer({
+  storage,
+});
 
 module.exports = upload;
